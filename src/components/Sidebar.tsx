c906ccf24f994dda68725d2d175c5f8f8d3326d6
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-08-28 08:34:28 +0000

    Added SIdebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+function Sidebar() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true); // Set the initial state to true
+
+    let handleSidebarToggle = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    }
+
+    return (
+        <div className={`w-64 transition-all duration-300 ease-in-out ${isSidebarOpen ? '' : 'w-16'}`}>
+            <div className="mb-8">
+                <svg onClick={handleSidebarToggle}
+                    className="w-6 h-6 text-white"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                    ></path>
+                </svg>
+
+            </div>
+            <div className={`mb-4 text-gray-300 text-2xl font-bold ${isSidebarOpen ? '' : 'hidden'}`}>
+                <p className='pb-10' >Home</p>
+                <p className='pb-10' >Videos</p>
+                <p className='pb-10'>Subscriptions</p>
+                <p className='pb-10' >Library</p>
+                <p className='pb-10'>Trending</p>
+            </div>
+        </div >
+    );
+}
+
+export default Sidebar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,34 @@
 import { Inter } from 'next/font/google'
 import VideoCard from '@/components/VideoCard'
 import AppBar from '@/components/AppBar'
+import Sidebar from '@/components/Sidebar' // Import the Sidebar component
 import data from '@/components/data'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <AppBar />
-      <div className='grid grid-cols-1 md:grid-cols-4 sm:grid-cols-3 '>
-        {
-          data.map(el => <VideoCard
-            key={Math.random}
-            title={el.title}
-            views={el.views}
-            thumbnail={el.thumbnail}
-            channelImg={el.channelImg}
-            uploadOn={el.uploadOn}
-            channel={el.channel}
-          >
+    <div className="flex">
+      <Sidebar />
+      <div className="flex flex-col w-full">
+        <AppBar />
+        <div className="p-5">
+          <div className='grid grid-cols-1 lg:grid-cols-4 sm:grid-cols-3 '>
+            {
+              data.map(el => <VideoCard
+                key={Math.random}
+                title={el.title}
+                views={el.views}
+                thumbnail={el.thumbnail}
+                channelImg={el.channelImg}
+                uploadOn={el.uploadOn}
+                channel={el.channel}
+              >
 
-          </VideoCard>)
-        }
-      </div >
-    </>
+              </VideoCard>)
+            }
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
