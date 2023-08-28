7b315d5de7bbceb1bd3a43a3f37fd538696c790d
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-08-28 05:25:41 +0000

    Initialized project using next.js

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function VideoCard() {
+    return (
+        <div className='rounded-xl'>
+            <img src="https://picsum.photos/id/237/300/160" />
+            <div>Hello World  </div>
+            <div>24K views<span><b>.</b>   </span></div>
+        </div>
+    )
+}
+
+export default VideoCard
